869bb07c10f2001bfe74643527f4900e3c715625
Christian Alexander Yana Huanca <cyanahu@unsa.edu.pe>
2024-12-11 16:02:00 -0500

    Se agrega la clase Mapa con constructor

---
+++ VideoJuego.java
@@ -0,0 +1,26 @@
+class Mapa {
+    private static final int TAMANIO = 10;
+    private Soldado[][] tablero;
+    private String territorio;
+    private Ejercito ejercito1;
+    private Ejercito ejercito2;
+    private Random random;
+
+    private final String[] TERRITORIOS = {
+        "Bosque",
+        "Campo Abierto",
+        "Montaña",
+        "Desierto",
+        "Playa"
+    };
+
+    public Mapa(Ejercito ejercito1, Ejercito ejercito2) {
+        this.ejercito1 = ejercito1;
+        this.ejercito2 = ejercito2;
+        this.tablero = new Soldado[TAMANIO][TAMANIO];
+        this.random = new Random();
+        this.territorio = TERRITORIOS[random.nextInt(TERRITORIOS.length)];
+        posicionarSoldados(ejercito1);
+        posicionarSoldados(ejercito2);
+        aplicarBonusTerritorio();
+    }
