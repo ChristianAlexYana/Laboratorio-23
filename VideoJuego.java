a48604f377bfef7b1208c332e7819c5ff7e287d7
unknown <chavezcunopaul@gmail.com>
2024-12-11 16:14:17 -0500

    Creamos el constructor Y algunos metodos

--- VideoJuego.java
+++ VideoJuego.java
@@ -3,3 +3,22 @@ class Ejercito{
     private String reino;
     private List<Soldado> soldados;
 }
+public Ejercito(String reino) {
+    this.reino = reino;
+    this.soldados = new ArrayList<>();
+}
+
+public String getReino() { return reino; }
+public List<Soldado> getSoldados() { return soldados; }
+
+public void agregarSoldado(Soldado soldado) {
+    soldados.add(soldado);
+}
+
+public void eliminarSoldado(Soldado soldado) {
+    soldados.remove(soldado);
+}
+
+public boolean estaVacio() {
+    return soldados.isEmpty();
+}
