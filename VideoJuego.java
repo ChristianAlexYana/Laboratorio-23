d8322545d91df338e6684ad31f22b59c931feb50
Gustavo <glinares@unsa.edu.pe>
2024-12-11 15:53:42 -0500

    Creando clase Soldado

---
+++ VideoJuego.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.List;       
+import java.util.ArrayList; 
+import java.util.*;
+abstract class Soldado {
+    protected String nombre;
+    protected int nivelVida;
+    protected int ataque;
+    protected int defensa;
+    protected int fila;
+    protected int columna;
+    protected String reino;
+
+    public Soldado(String nombre, int nivelVida, int ataque, int defensa, String reino) {
+        this.nombre = nombre;
+        this.nivelVida = nivelVida;
+        this.ataque = ataque;
+        this.defensa = defensa;
+        this.reino = reino;
+    }
+
+    public String getNombre() { return nombre; }
+    public int getNivelVida() { return nivelVida; }
+    public void setNivelVida(int nivelVida) { this.nivelVida = nivelVida; }
+    public int getAtaque() { return ataque; }
+    public int getDefensa() { return defensa; }
+    public int getFila() { return fila; }
+    public void setFila(int fila) { this.fila = fila; }
+    public int getColumna() { return columna; }
+    public void setColumna(int columna) { this.columna = columna; }
+    public String getReino() { return reino; }
+
+    public abstract String representar();
+
+    public void aplicarBonus() {
+        this.nivelVida += 1;
+    }
+}
